b1d9a69e0afe2a0f16e44d33ae4954ae0acb7103
cynthiazeya <cynthia.zeya@edu.fh-joanneum.at>
2021-04-25 00:59:17 +0200

    :bug: fix function weightChange

--- app/controllers/cat.controller.js
+++ app/controllers/cat.controller.js
@@ -141,14 +141,13 @@ exports.load = (req, res, next, id) =>
 
 
 function weightChange(req, res, weight) {
-        if(req.cat.weight + weight > 8 || req.cat.weight - weight < 2) {
-            if(weight > 0) {
+            if(weight > 0 && req.cat.weight + weight > 8 ) {
                 res.status(500).send({message:`${req.cat.name} needs to lose weight`});
+                throw new Error('Could not update Cat');
             }
-            if (weight < 0) {
+            if (weight < 0 && req.cat.weight - weight < 2) {
                 res.status(500).send({message:`${req.cat.name} needs to eat`});
+                throw new Error('Could not update Cat');
             }
-            throw new Error('Could not update Cat');
-        }
         req.body.weight = Math.round((req.cat.weight+weight)*100)/100;
 }
