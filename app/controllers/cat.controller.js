8043646eeea562aad965e06bae2b2e3e27658b6a
konrab-ima <birgit.konrad@edu.fh-joanneum.at>
2021-04-25 00:37:05 +0200

    :construction: send tags

--- app/controllers/cat.controller.js
+++ app/controllers/cat.controller.js
@@ -18,7 +18,7 @@ exports.create = (req, res) => {
 // Retrieve and return all cats from the database.
 exports.getAllTags = () => {
     Cat.find()
-        .select('tags')
+        // .select('tags')
         // .then(cats => cats.map(c => c.tags))
     //     .then(tags => _.uniq(_.flatten(tags)))
         .then(uniqueTags => res.send(uniqueTags))
