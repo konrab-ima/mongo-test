d0fd4a7ddc9dd60867954bc3bdb85e075f89e205
konrab-ima <birgit.konrad@edu.fh-joanneum.at>
2021-04-26 18:33:57 +0200

    :bug: tags virtual

--- app/models/cat.model.js
+++ app/models/cat.model.js
@@ -22,7 +22,15 @@ const CatSchema = mongoose.Schema({
         max: 8
     }
 }, {
-    timestamps: true
+    timestamps: true,
+    toJSON: {virtuals: true}
 });
+
+CatSchema.virtual('tags').get(function(){
+    let regexp = new RegExp('#[A-Za-z0-9]*', 'g');
+    if (!this.description) return [];
+    const hashtags = this.description.match(regexp) || [];
+    return _.uniq(hashtags.map(ht => ht.substring(1)));
+})
 
 module.exports = mongoose.model('cat', CatSchema);
